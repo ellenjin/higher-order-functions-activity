6ff6ee63ae84c24179d923d470bfd62f34adfa33
Solhee <ellen.solhee.jin@gmail.com>
2025-04-04 09:28:33 -0700

    Implemented all functions

--- main.py
+++ main.py
@@ -10,8 +10,18 @@ WORDS = ["jumps", "laziest", "brown", "a", "quick", "fox", "the", "dog", "over"]
 # comparison value as determined by calling the function passed in the key
 # parameter on it. This will be very similar to the min_function_custom
 # developed in the Learn reading.
+
 def my_max(collection, key):
-    pass
+    try: 
+        max_item = collection[0]
+        max_val = key(collection[0])
+        for item in collection:
+            if key(item) > max_val:
+                max_item = item
+                max_val = key(item)
+        return max_item
+    except ValueError as e:
+        print(f"Value Error: {e}")
 
 # Implement a custom version of filter, called my_filter
 # my_filter takes a function (should_keep) which it will call on every item in
@@ -22,7 +32,15 @@ def my_max(collection, key):
 def my_filter(should_keep, collection):
     # if you've encountered list comprehensions, this would be a
     # great place to use one
-    pass
+    # to_keep = []
+
+    # for item in collection:
+    #     if should_keep(item):
+    #         to_keep.append(item)
+
+    # return to_keep
+
+    return [item for item in collection if should_keep(item)]
 
 # Implement a custom version of map, called my_map
 # my_map takes a function (transform) which it will call on every item in the
@@ -31,9 +49,7 @@ def my_filter(should_keep, collection):
 # should be returned.
 # WARNING: notice the parameters order is reversed compared to my_max
 def my_map(transform, collection):
-    # if you've encountered list comprehensions, this would be a
-    # great place to use one
-    pass
+    return [transform(item) for item in collection]
 
 #################################################
 # NO CODE BELOW THIS POINT NEEDS TO BE MODIFIED #
